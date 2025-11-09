f25baff20155f65065be70a1f3952387260b1d4f
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-11-09 07:11:20 +0200

    add tiptap editor extensions

--- app/documents/[documentId]/editor.tsx
+++ app/documents/[documentId]/editor.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { TaskList, TaskItem } from "@tiptap/extension-list";
+import { TableKit } from "@tiptap/extension-table";
+import Image from "@tiptap/extension-image";
 
 export const Editor = () => {
   const editor = useEditor({
@@ -11,9 +14,26 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World!</p>",
-    immediatelyRender: true,
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({ nested: true }),
+      TableKit.configure({ table: { resizable: true } }),
+      Image.configure({
+        resize: {
+          enabled: true,
+          directions: ["top", "bottom", "left", "right"], // can be any direction or diagonal combination
+          minWidth: 50,
+          minHeight: 50,
+          alwaysPreserveAspectRatio: true,
+        },
+        allowBase64: true,
+      }),
+    ],
+    content: `
+        <p>This is a basic example of implementing images. Drag to re-order.</p>
+      `,
+    immediatelyRender: false,
   });
   return (
     <div className="size-full overflow-x-auto bg-[#F9fBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
