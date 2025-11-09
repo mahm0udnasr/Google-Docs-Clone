cfc3ba2c7e36adfc3c6160dd12ed3a580e9a773f
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-11-09 06:18:35 +0200

    add doc by id and editor

---
+++ app/documents/[documentId]/page.tsx
@@ -0,0 +1,15 @@
+import { Editor } from "./editor";
+interface DocumentPageProps {
+  params: Promise<{ documentId: string }>;
+}
+
+const DocumentPage = async ({ params }: DocumentPageProps) => {
+  const documentId = (await params).documentId;
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Editor />
+    </div>
+  );
+};
+
+export default DocumentPage;
