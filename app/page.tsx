a6510ca4704f72404694c89d64f7531378b3e76e
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-11-19 07:22:37 +0200

    delete page from main route

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Link href={"/documents/123"}>Document</Link>
-    </div>
-  );
-}
