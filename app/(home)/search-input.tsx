80565f4c297b8fbb1433d9f0073d9500e1edf4e8
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-11-20 06:27:48 +0200

    implement the search hook

--- app/(home)/search-input.tsx
+++ app/(home)/search-input.tsx
@@ -3,20 +3,28 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SearchIcon, XIcon } from "lucide-react";
 import { useRef, useState } from "react";
+import { useSearchParam } from "@/hooks/use-search-param";
 
 export const SearchInput = () => {
-  const [value, setValue] = useState<string>("");
+  const [search, setSearch] = useSearchParam();
+  const [value, setValue] = useState<string>(search);
   const inputRef = useRef<HTMLInputElement>(null);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
   const handleClear = () => {
     setValue("");
+    setSearch("");
+    inputRef.current?.blur();
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch(value);
     inputRef.current?.blur();
   };
   return (
     <div className="flex-1 flex items-center justify-center">
-      <form className="relative max-w-[720px] w-full">
+      <form className="relative max-w-[720px] w-full" onSubmit={handleSubmit}>
         <Input
           placeholder="Search"
           className="md:text-base placeholder:text-neutral-800 px-14 w-full border-none focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3px_1px_rgba(65,69,73,.15)] bg-[#f0f4f8] rounded-full h-12 focus-visible:ring-0 focus:bg-white"
