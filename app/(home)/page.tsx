e557f713e8b486f5c0e5e06e3654746d60ceac06
mahm0udnasr <contact.mahmoudnasr@gmail.com>
2025-11-22 07:19:16 +0200

    install convex

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./templates-gallery";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const documents = useQuery(api.documents.get);
+  if (documents === undefined) {
+    return <p>Loading...</p>;
+  }
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +16,9 @@ export default function Home() {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   );
